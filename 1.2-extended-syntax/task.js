fb4990e7e4d2fb3f7cfd6085ef3e6f35cb80658d
Alexey Shishkin <shayu78@mail.ru>
2020-06-12 18:08:12 +0400

    Решение домашнего задания 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,41 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+"use strict";
+
+function getResult(a, b, c) {
+    let discriminant = b ** 2 - 4 * a * c;
+    let x = [];
+
+    if (discriminant === 0) {
+        x.push(-b / (2 * a));
+    } else if (discriminant > 0) {
+        x.push((-b + Math.sqrt(discriminant)) / (2 * a));
+        x.push((-b - Math.sqrt(discriminant)) / (2 * a));
+    }
+
+    return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    let averageMark = 0;
+    let countMarks = marks.length;
+    if (countMarks === 0) return averageMark;
+    if (countMarks > 5) {
+        alert('Количество оценок больше допустимого. Оставляем первые пять');
+        marks = marks.slice(0, 5);
+        countMarks = 5;
+    }
+    averageMark = marks.reduce(function (averageMark, currentValue) {
+        return averageMark + currentValue;
+    }, averageMark) / countMarks;
+
+    return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+    let currYear = new Date().getFullYear();
+    let personBirthYear = dateOfBirthday.getFullYear();
+    let message = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+
+    if (currYear - personBirthYear >= 18) message = `Не желаете ли олд-фэшн, ${name}?`;
+
+    return message;
 }
